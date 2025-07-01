89757ec6f3e5e62a21f0dd4e2f4827f1a23edf55
heloisapazini <137806412+heloisapazini@users.noreply.github.com>
2025-07-01 08:41:13 -0300

    oi

--- main.js
+++ main.js
@@ -71,7 +71,11 @@ console.log(alfabeto);
 }
 function classificaSenha(){
     forcaSenha.classList.remove('fraca','media','forte');
-    forcaSenha.classList.add('forte');
-
-
-}
+    if (tamanhoSenha > 11){ 
+        forcaSenha.classList.add('forte');
+     } else if (tamanhoSenha > 5 && tamanhoSenha < 12 ){
+     forcaSenha.classList.add('media');
+     }else if (tamanhoSenha <= 5){
+        forcaSenha.classList.add('fraca');
+     }
+ }
